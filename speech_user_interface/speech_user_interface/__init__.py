02c0fc2a81a7934ab152d8cd52c9a0db19a5bdd1
David Piper <dbpiper@gmail.com>
2024-04-13 00:17:43 -0500

    Create program to interact with computer with speech

---
+++ speech_user_interface/speech_user_interface/__init__.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+
+from .read_in_speech import read_in_speech
+from .send_text_to_chatgpt import send_text_to_chatgpt
+from .speak_text import speak_text
+from .compare_strings import compare_strings
+from .load_vosk_model import load_vosk_model
+
+
+__name__ == "__main__"
+__all__ = []
+
+
+def main():
+    load_dotenv()
+    vosk_model = load_vosk_model()
+    # read in speech from the user as a sound file
+    # process that speech and convert it to text
+    if vosk_model:
+        while True:
+            speech_text = read_in_speech(vosk_model)
+            if compare_strings(speech_text, "exit the program") > 0.9:
+                break
+
+            if isinstance(speech_text, str):
+                print("speech_text:", speech_text)
+                reponse_text = send_text_to_chatgpt(speech_text)
+                speak_text(reponse_text)
+
+    # use the text to talk to ChatGPT
+    # take ChatGPT's response and convert that to speech
